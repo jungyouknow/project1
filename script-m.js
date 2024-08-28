63859abdac6721239b98ef6293c21bf2bcb90496
jungyoonho <130973189+jungyouknow@users.noreply.github.com>
2024-08-29 08:56:41 +0900

    Add files via upload

--- script-m.js
+++ script-m.js
@@ -1,11 +1,25 @@
+//모바일 헤더 열고 닫기
+
 const nav = document.getElementById("nav");
 
-// 메뉴 열기
 function openMenu() {
     nav.style.width = '289px';
 }
 
-// 메뉴 닫기
 function closeMenu() {
     nav.style.width = '0px';
 }
+
+function validateEmail() {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    let emailInput = document.getElementById("email").value;
+
+    if (emailInput === "") {
+        alert("이메일을 입력하세요.");
+    } else if (!emailPattern.test(emailInput)) {
+        alert("유효한 이메일 형식을 입력하세요.");
+    } else {
+        modalOn();
+    }
+}
+
